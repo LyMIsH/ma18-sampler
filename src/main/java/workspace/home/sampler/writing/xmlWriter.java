5bb6a4448a591620f3a4c6f1e635588e7e996271
Lymish <ronuthup@gmail.com>
2022-01-17 17:52:07 +0200

    Push before merge, added initial next feature file

---
+++ src/main/java/workspace/home/sampler/writing/xmlWriter.java
@@ -0,0 +1,12 @@
+package workspace.home.sampler.writing;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class xmlWriter implements Writable {
+    @Override
+    public void write(String destFolder, List<HashMap<String, String>> recordStream) throws IOException {
+        
+    }
+}
