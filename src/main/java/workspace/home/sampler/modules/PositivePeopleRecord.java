ffa3f87d88da90fb44a241b20f33c2fcc62a803b
Lymish <ronuthup@gmail.com>
2022-01-18 10:13:01 +0200

    Added merged record class

---
+++ src/main/java/workspace/home/sampler/modules/PositivePeopleRecord.java
@@ -0,0 +1,21 @@
+package workspace.home.sampler.modules;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PositivePeopleRecord extends Record {
+
+    public PositivePeopleRecord()
+    {
+        super();
+        this.columns = new ArrayList<>(Arrays.asList("MDACODE",
+                "IDNum", "IDType", "FirstName", "LastName", "City", "Street", "BuildingNumber",
+                "Barcode", "BirthDate", "Labcode", "ResultDate", "TakeDate", "StickerNumber",
+                "ResultTestCorona", "Variant", "TestType"));
+    }
+
+    @Override
+    public Record create() {
+        return new PositivePeopleRecord();
+    }
+}
